6019876d71a8bb4adaaeb2afc4179e723dfa1360
dk.cho <drivvdry@gmail.com>
2024-07-19 17:05:28 +0900

    add Health checking

--- src/client.py
+++ src/client.py
@@ -2,15 +2,35 @@ import grpc
 from src.grpc import grpc_pb2_grpc
 from src.grpc import grpc_pb2
 from src.grpc.Id_Manager import Id_Manager
+from grpc_health.v1 import health_pb2
+from grpc_health.v1 import health_pb2_grpc
+from time import sleep
+
+def health_check_call(stub: health_pb2_grpc.HealthStub):
+    request = health_pb2.HealthCheckRequest(service="helloworld.Greeter")
+    resp = stub.Check(request)
+    if resp.status == health_pb2.HealthCheckResponse.SERVING:
+        print("server is serving")
+    elif resp.status == health_pb2.HealthCheckResponse.NOT_SERVING:
+        print("server stopped serving")
+
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = grpc_pb2_grpc.Id_ManagerStub(channel)
+        health_stub = health_pb2_grpc.HealthStub(channel)
         stub.setId(grpc_pb2.req(id="1001"))
         res = stub.getId(grpc_pb2.req(id=None))
         print(res.id)
+
+        # Check health status every 1 second for 30 seconds
+        for _ in range(30):
+            health_check_call(health_stub)
+            sleep(1)
+
+    
     
 
 if __name__ =="__main__":
--- src/server.py
+++ src/server.py
@@ -1,15 +1,54 @@
 import grpc
+import grpc_admin
+from src.grpc import grpc_pb2
 from src.grpc import grpc_pb2_grpc
 from src.grpc.Id_Manager import Id_Manager
 from concurrent import futures
+import threading
+from time import sleep
 import logging
 
+from grpc_health.v1 import health
+from grpc_health.v1 import health_pb2
+from grpc_health.v1 import health_pb2_grpc
+
+
 logging.basicConfig(level=logging.INFO)
+
+
+def _toggle_health(health_servicer: health.HealthServicer, service: str):
+    next_status = health_pb2.HealthCheckResponse.SERVING
+    while True:
+        if next_status == health_pb2.HealthCheckResponse.SERVING:
+            next_status = health_pb2.HealthCheckResponse.NOT_SERVING
+        else:
+            next_status = health_pb2.HealthCheckResponse.SERVING
+
+        health_servicer.set(service, next_status)
+        sleep(5)
+
+
+def _configure_health_server(server: grpc.Server):
+    health_servicer = health.HealthServicer(
+        experimental_non_blocking=True,
+        experimental_thread_pool=futures.ThreadPoolExecutor(max_workers=10),
+    )
+    health_pb2_grpc.add_HealthServicer_to_server(health_servicer, server)
+
+    # Use a daemon thread to toggle health status
+    toggle_health_status_thread = threading.Thread(
+        target=_toggle_health,
+        args=(health_servicer, "helloworld.Greeter"),
+        daemon=True,
+    )
+    toggle_health_status_thread.start()
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     grpc_pb2_grpc.add_Id_ManagerServicer_to_server(Id_Manager(), server)
     server.add_insecure_port("[::]:50051")
+    grpc_admin.add_admin_servicers(server)
+    _configure_health_server(server)
     server.start()
     server.wait_for_termination()
     
